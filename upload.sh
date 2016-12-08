d2a1cf19ffabbb7f7d38bcd6756218a23271fad5
MrDoomLittle <x-master-of-disaster-x@hotmail.com>
2016-12-08 20:04:41 +0000

    .

--- upload.sh
+++ upload.sh
@@ -1,1 +1,1 @@
-avrdude -C avrdude.conf -v -v -v -v -p atmega328p -c arduino -P/dev/ttyUSB0 -b 115200 -D -Uflash:w:main.hex:i
+avrdude -C avrdude.conf -v -v -v -v -p atmega328p -c arduino -P/dev/ttyUSB0 -b 115200 -D -Uflash:w:micro.hex:i
